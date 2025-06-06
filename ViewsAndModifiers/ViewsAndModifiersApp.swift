981f4bc4c4645f74c652e74e714c78b11f0e77f2
Alonso Acosta <alonso.ae@hotmail.com>
2025-06-06 11:33:13 -0600

    Complete the lesson of modifiers

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Alonso Acosta Enriquez on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
